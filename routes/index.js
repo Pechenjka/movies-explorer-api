adde6affa258f9c19e8a54315b6c443a2dd25ae9
Petr Lobachev <petya.lobachev@yandex.ru>
2021-03-11 23:31:26 +0300

    feat: добавить роутеры для регистрации и авторизации

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ const router = require('express').Router();
 const usersRoutes = require('./users');
 const moviesRoutes = require('./movies');
 
+router.post('/signup', Register);
+router.post('/signin', Login);
 router.use('/users', usersRoutes);
 router.use('/movies', moviesRoutes);
 
