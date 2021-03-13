590685ade3827efed9733398482ffd7d64fc3955
Petr Lobachev <petya.lobachev@yandex.ru>
2021-03-13 18:18:00 +0300

    feat: добавить логгеры запросов и ошибок

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 
 const { PORT, MONGO_URL } = require('./config');
 const router = require('./routes/index');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const app = express();
 
@@ -18,6 +19,8 @@ mongoose.connection.on('open', () => console.log('Mongoose connection...'));
 app.use(cors());
 app.use(express.json({ extended: true }));
 
+app.use(requestLogger);
 app.use('/', router);
+app.use(errorLogger);
 
 app.listen(PORT, () => console.log(`Aplication is working on the port ${PORT}`));
