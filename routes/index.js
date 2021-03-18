f4dfa1b1b5b8e969d2586b8fb918ab89be06315f
Petr Lobachev <petya.lobachev@yandex.ru>
2021-03-18 19:31:25 +0300

    fix: добавить мидлваре-обработчик несуществующего маршрута

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,13 @@ const moviesRoutes = require('./movies');
 const { createUser, login } = require('../controllers/users');
 const auth = require('../middlewares/auth');
 const { validationRegisterUser, validationAuthorizationUser } = require('../middlewares/validation');
+const { NotFound } = require('../error');
 
 router.post('/signup', validationRegisterUser, createUser);
 router.post('/signin', validationAuthorizationUser, login);
 router.use('/users', auth, usersRoutes);
 router.use('/movies', auth, moviesRoutes);
-
+router.use('*', () => {
+  throw new NotFound('Запрошенный ресурс не найден');
+});
 module.exports = router;
