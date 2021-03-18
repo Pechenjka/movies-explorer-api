0b33671e40e93829c9b677e794ccf629e2d0a02f
Petr Lobachev <petya.lobachev@yandex.ru>
2021-03-18 18:50:50 +0300

    fix: исправить вывод ошибок авторизации

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,17 +1,18 @@
 const jwt = require('jsonwebtoken');
 const { JWT_SECRET } = require('../config');
+const { Unauthorized } = require('../error');
 
 const auth = (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization) {
-    Promise.reject(new Error('нет токена'));
+    throw new Unauthorized('нет токена');
   }
   const token = authorization.replace('Bearer ', '');
   let payload;
   try {
     payload = jwt.verify(token, JWT_SECRET);
   } catch (err) {
-    Promise.reject(new Error('нет токена'));
+    throw new Unauthorized('нет токена');
   }
   req.user = payload;
 
