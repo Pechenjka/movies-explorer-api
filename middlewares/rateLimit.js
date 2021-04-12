f109b339808e8e5b34bf54bd7ec4a39d5dae6b9a
Petr Lobachev <petya.lobachev@yandex.ru>
2021-04-12 23:26:36 +0300

    feat изменил настройки rate-limiter

--- middlewares/rateLimit.js
+++ middlewares/rateLimit.js
@@ -1,8 +1,8 @@
 const rateLimit = require('express-rate-limit');
 
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 минут
-  max: 100, // Ограничить запросы на сервер в кол-ве - 100 ед
+  windowMs: 1 * 60 * 1000, // 1 минута
+  max: 200, // Ограничить запросы на сервер в кол-ве - 200 ед
 });
 
 module.exports = limiter;
